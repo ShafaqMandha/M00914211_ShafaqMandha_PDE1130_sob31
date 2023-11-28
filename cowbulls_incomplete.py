d898917732a2f0ee4a055b85e69c5cfb4f03d2fd
ShafaqMandha <shafaqmandha@gmail.com>
2023-11-28 13:51:21 +0400

    Task 4 - Fixed all the errors

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -25,7 +25,7 @@ def compare_numbers(number, user_guess):
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+print(number) # Changed "print number" to "print(number)"
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -34,7 +34,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!") # Changed "raw_input" to "input"
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
