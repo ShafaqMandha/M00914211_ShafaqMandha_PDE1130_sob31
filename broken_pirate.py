23148e7cdd18f87736a8bc06a0702dfe88a124ea
ShafaqMandha <shafaqmandha@gmail.com>
2023-11-28 11:40:23 +0400

    Task 2 - Final Updated Version

--- broken_pirate.py
+++ broken_pirate.py
@@ -7,6 +7,16 @@
 greeting = input("Hello, possible pirate! What's the password?") # Added a double quote to end the input prompt (afater "password?").
 if greeting == "Arrr!":
 	print("Go away, pirate.")
+	'''
+		Corrections made:
+			- Removed the parenthesis. 
+			- Changed "in" to "==" so when the input is == to Arrr!. Then the out given would be Go away, pirate. 
+	'''
 else:
 	print("Greetings, hater of pirates!")
-
+	'''
+		Corrections made:
+			-	Changed "elif" to "else".
+			-	Added a colon after else. 
+			-	Intended the print statement after the else statement to follow the proper syntax.
+	'''
