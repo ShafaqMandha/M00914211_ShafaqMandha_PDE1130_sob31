397c7f2c7c89347a383539e0e7e2f1c46f7931f9
ShafaqMandha <shafaqmandha@gmail.com>
2023-11-28 11:34:11 +0400

    Task 2 - Before error corrections

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,3 +1,9 @@
+'''
+    Name: Shafaq Mandha 
+    MISIS: M00914211
+    Module: PDE1130
+    SOB no. 31
+'''
 greeting = input("Hello, possible pirate! What's the password?)
 if greeting in ["Arrr!"):
 	print("Go away, pirate.")
